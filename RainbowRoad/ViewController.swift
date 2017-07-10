aba6ff36ab86733def40730b5c83e6891c541ba3
Robert Villarreal <me@robertv.co>
2017-07-10 12:12:31 -0700

    initial and final commit

--- RainbowRoad/ViewController.swift
+++ RainbowRoad/ViewController.swift
@@ -8,18 +8,29 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
+    
+   
+    
+    var colors = [UIColor.red, UIColor.orange, UIColor.yellow, UIColor.green,UIColor.blue,UIColor.purple]
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return colors.count
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MyCell", for: indexPath)
+        cell.backgroundColor = colors[indexPath.row]
+        return cell
     }
-
-
+    
+    
+    
 }
 
